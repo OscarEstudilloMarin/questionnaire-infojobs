7e0e2c6c217b914eef5208c60769fd64813d16ab
obnol <longboqiu@hotmail.com>
2023-05-26 14:03:35 +0200

    Add `offer` dynamic route

---
+++ app/offers/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function OfferPage({
+    params: { id },
+}: {
+    params: { id: string }
+}) {
+    return <div>Offer!! + {id}</div>
+}
--- components/offer-card.tsx
+++ components/offer-card.tsx
@@ -1,12 +1,8 @@
 import { Offer } from '@/lib/collection'
-import {
-    Card,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card'
+import { Card, CardDescription, CardTitle } from '@/components/ui/card'
 import { Separator } from './ui/separator'
 import { Skeleton } from './ui/skeleton'
+import Link from 'next/link'
 
 type OfferCardProps = {
     offer?: Offer
@@ -34,48 +30,50 @@ const OfferCard = ({ offer, skeleton }: OfferCardProps) => {
             </div>
         </Card>
     ) : offer ? (
-        <Card className="flex w-full cursor-pointer items-center gap-5 bg-white p-5 transition-colors hover:bg-accent">
-            <div className="h-fit w-1/4 rounded-md border-[1px] border-solid border-border bg-white">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                    src={
-                        offer.author.logoUrl ||
-                        'https://components.infojobs.com/statics/images/pic-company-logo.png'
-                    }
-                    alt={offer.author.name + ' icon'}
-                    className="h-full w-full rounded-md object-cover"
-                />
-            </div>
-            <div className="flex h-full w-3/4 flex-col items-start justify-between py-2">
-                <div>
-                    <CardTitle>{offer.title}</CardTitle>
-                    <CardDescription>{offer.author.name}</CardDescription>
+        <Link href={`/offers/${offer.id}`}>
+            <Card className="flex w-full cursor-pointer items-center gap-5 bg-white p-5 transition-colors hover:bg-accent">
+                <div className="h-fit w-1/4 rounded-md border-[1px] border-solid border-border bg-white">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                        src={
+                            offer.author.logoUrl ||
+                            'https://components.infojobs.com/statics/images/pic-company-logo.png'
+                        }
+                        alt={offer.author.name + ' icon'}
+                        className="h-full w-full rounded-md object-cover"
+                    />
                 </div>
+                <div className="flex h-full w-3/4 flex-col items-start justify-between py-2">
+                    <div>
+                        <CardTitle>{offer.title}</CardTitle>
+                        <CardDescription>{offer.author.name}</CardDescription>
+                    </div>
 
-                <div className="flex items-center justify-between gap-2">
-                    {offer.city && (
-                        <span className="... max-w-[125px] truncate whitespace-nowrap text-xs text-gray-400">
-                            {offer.city}
-                        </span>
-                    )}
-                    <Separator orientation="vertical" />
-                    {offer.teleworking && (
-                        <span className="text-xs text-gray-400">
-                            {offer.teleworking.value}
-                        </span>
-                    )}
-                    {offer.salaryMin.value && offer.salaryMax.value ? (
-                        <>
-                            <Separator orientation="vertical" />
-                            <span className="text-xs text-gray-400">
-                                {offer.salaryMin.value} -{' '}
-                                {offer.salaryMax.value}
+                    <div className="flex items-center justify-between gap-2">
+                        {offer.city && (
+                            <span className="... max-w-[125px] truncate whitespace-nowrap text-xs text-gray-400">
+                                {offer.city}
                             </span>
-                        </>
-                    ) : null}
+                        )}
+                        <Separator orientation="vertical" />
+                        {offer.teleworking && (
+                            <span className="text-xs text-gray-400">
+                                {offer.teleworking.value}
+                            </span>
+                        )}
+                        {offer.salaryMin.value && offer.salaryMax.value ? (
+                            <>
+                                <Separator orientation="vertical" />
+                                <span className="text-xs text-gray-400">
+                                    {offer.salaryMin.value} -{' '}
+                                    {offer.salaryMax.value}
+                                </span>
+                            </>
+                        ) : null}
+                    </div>
                 </div>
-            </div>
-        </Card>
+            </Card>
+        </Link>
     ) : null
 }
 
