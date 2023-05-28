466b44a664205a90e967a4525ab1dbbc5410216f
obnol <longboqiu@hotmail.com>
2023-05-28 17:39:14 +0200

    Improve `questionnaire`

--- components/questionnaire.tsx
+++ components/questionnaire.tsx
@@ -5,13 +5,23 @@ import { useState } from 'react'
 import { Textarea } from './ui/textarea'
 import { Icons } from './icons'
 import { toast } from '@/hooks/use-toast'
-import { Input } from './ui/input'
+import { Card } from './ui/card'
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
 
 export default function Questionnaire() {
     const [input, setInput] = useState('')
-    const [number, setNumber] = useState(5)
+    const [number, setNumber] = useState<null | number>(null)
     const [loading, setLoading] = useState(false)
     const [questions, setQuestions] = useState<string[]>([])
+
+    console.log('NUMBER', number)
 
     const handleSubmit = async (e: any) => {
         try {
@@ -47,14 +57,29 @@ export default function Questionnaire() {
             <form onSubmit={handleSubmit}>
                 <div className="flex items-center space-x-5">
                     <Textarea
-                        placeholder="Descripción"
+                        placeholder="Preguntas sobre useEffect, useState y useMemo de nivel junior"
                         onChange={(e) => setInput(e.target.value)}
                         value={input}
                     />
-                    <Input
-                        value={number}
-                        onChange={(e) => setNumber(Number(e.target.value))}
-                    />
+                    <Select
+                        onValueChange={(value) => setNumber(Number(value) + 1)}
+                    >
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Número de preguntas" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                {Array.from(Array(5).keys()).map((item) => (
+                                    <SelectItem
+                                        key={item}
+                                        value={item.toString()}
+                                    >
+                                        {item + 1}
+                                    </SelectItem>
+                                ))}
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
                     <Button type="submit" disabled={loading}>
                         {loading && (
                             <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
@@ -64,15 +89,15 @@ export default function Questionnaire() {
                 </div>
             </form>
             {questions.length > 0 && (
-                <div>
-                    <h2 className="mt-5 text-2xl font-bold">Preguntas</h2>
-                    <ul className="mt-5">
+                <div className="space-y-3">
+                    <p className="mt-5 text-xl font-bold">Preguntas</p>
+                    <div className="space-y-2">
                         {questions.map((question, index) => (
-                            <li key={index} className="text-lg">
-                                {question}
-                            </li>
+                            <Card key={index} className="p-2">
+                                <p>{question}</p>
+                            </Card>
                         ))}
-                    </ul>
+                    </div>
                 </div>
             )}
         </div>
