e6afbb071563147c02d31bc5039451b106b0d0bc
Oscar Estudillo <oscar.estudillo.marin@gmail.com>
2023-05-30 18:04:32 +0200

    Fix offer card

--- components/offer-card.tsx
+++ components/offer-card.tsx
@@ -20,20 +20,27 @@ const OfferCard = ({ offer, skeleton }: OfferCardProps) => {
 
     return skeleton ? (
         <Card className="flex w-full cursor-pointer items-center gap-5 bg-white p-5 transition-colors hover:bg-accent">
-            <div className="h-fit w-1/4 rounded-md border-[1px] border-solid border-border bg-white">
-                <Skeleton className="h-28 w-full rounded-md" />
+            <div className="aspect-square h-fit w-1/4 rounded-md border-[1px] border-solid border-border bg-white">
+                <Skeleton className="h-full w-full rounded-md" />
             </div>
-            <div className="flex h-full w-3/4 flex-col items-start justify-between py-2">
-                <div className="flex flex-col gap-2">
-                    <Skeleton className="h-5 w-60" />
-                    <Skeleton className="h-3 w-32" />
+            <div className="flex h-full w-3/4 flex-col items-start justify-between gap-2 py-2">
+                <div className="flex w-full flex-col gap-2">
+                    <Skeleton className="h-5 w-[80%]" />
+                    <Skeleton className="h-3 w-[70%]" />
                 </div>
-                <div className="flex items-center justify-between gap-2">
-                    <Skeleton className="h-3 w-24" />
-                    <Separator orientation="vertical" />
-                    <Skeleton className="h-3 w-28" />
-                    <Separator orientation="vertical" />
-                    <Skeleton className="h-3 w-24" />
+                <div className="flex w-full flex-col gap-2">
+                    <div className="flex w-full items-center gap-2">
+                        <Skeleton className="h-3 w-[20%]" />
+                        <Separator orientation="vertical" />
+                        <Skeleton className="h-3 w-[22%]" />
+                        <Separator orientation="vertical" />
+                        <Skeleton className="h-3 w-[25%]" />
+                    </div>
+                    <div className="flex w-full items-center  gap-2">
+                        <Skeleton className="h-3 w-[30%]" />
+                        <Separator orientation="vertical" />
+                        <Skeleton className="h-3 w-[20%]" />
+                    </div>
                 </div>
             </div>
         </Card>
@@ -43,9 +50,9 @@ const OfferCard = ({ offer, skeleton }: OfferCardProps) => {
             onClick={() => router.push(`/offers/${offer.id}`)}
         >
             <div
-                className={`h-fit w-1/4 ${
+                className={`flex aspect-square w-1/4 items-center justify-center rounded-md ${
                     !offer?.user.image
-                        ? 'rounded-md border-[1px] border-solid border-border'
+                        ? ' border-[1px] border-solid border-border'
                         : ''
                 } bg-white`}
             >
@@ -56,7 +63,7 @@ const OfferCard = ({ offer, skeleton }: OfferCardProps) => {
                         'https://components.infojobs.com/statics/images/pic-company-logo.png'
                     }
                     alt={'icon'}
-                    className="h-full w-full rounded-md object-cover"
+                    className="h-full rounded-md object-cover"
                 />
             </div>
             <div className="flex h-full w-3/4 flex-col items-start justify-between py-2">
