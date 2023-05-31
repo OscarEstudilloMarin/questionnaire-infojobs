40d912403c43e1eceb01cd2fd6fa1887bb8e20b9
obnol <longboqiu@hotmail.com>
2023-05-31 09:29:12 +0200

    Add how-to-test

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,13 @@ import { Database } from '@/lib/database.types'
 
 import CreateOfferBtn from './create-offer-btn'
 import OffersGrid from '@/components/offers-grid'
+import { Info } from 'lucide-react'
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from '@/components/ui/popover'
+import { Button } from '@/components/ui/button'
 
 export default async function Home() {
     const supabase = createServerComponentClient<Database>({ cookies })
@@ -18,6 +25,17 @@ export default async function Home() {
 
     return (
         <main className="flex w-full flex-col gap-4 overflow-y-auto p-10">
+            <Popover>
+                <PopoverTrigger asChild>
+                    <Button variant="secondary" className="w-fit">
+                        Cómo probar
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent align="end">
+                    Place content for the popover here.
+                </PopoverContent>
+            </Popover>
+
             <div className="mb-4 flex flex-row justify-between">
                 <h1 className="text-3xl font-bold">Ofertas</h1>
                 {user?.type === 'employer' && <CreateOfferBtn />}
